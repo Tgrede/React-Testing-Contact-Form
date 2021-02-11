4a583d35fc0e1e0438d28516b709c9ee3e04e52f
Trenten grede <Trenten.grede@gmail.com>
2021-02-11 16:51:40 -0600

    initial commit

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {} from '@testing-library/react'
+import {} from '@testing-library/user-event'
+import ContactForm from './ContactForm'
+
+test('sanity test', () => {
+     expect(3+3).not.toBe(7)
+     expect(3+3).toBe(6)
+})
